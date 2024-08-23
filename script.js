312b7fe2054d61490d55c128772dc8918eb848a2
AbhaySengar27 <64154313+AbhaySengar27@users.noreply.github.com>
2024-08-23 18:19:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+function showTab(tabId) {
+    // Hide all tab contents
+    document.querySelectorAll('.tab-content').forEach((tab) => {
+        tab.style.display = 'none';
+    });
+
+    // Show the selected tab content
+    document.getElementById(tabId).style.display = 'block';
+}
